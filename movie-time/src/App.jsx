20362c3b0b67244516d052c74a6b03e1b320d57d
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-10 20:23:34 +0530

    react-router-dom, assets, basic setup

--- movie-time/src/App.jsx
+++ movie-time/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "./App.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ movie-time/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div className="HomePage">
+      <Header />
+      {/* <div className="HeroSeciton">
+        <h1>Welcome.</h1>
+        <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
+        <input type="text" placeholder="Search for a movie or tv show..."/>
+      </div> */}
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Home;
