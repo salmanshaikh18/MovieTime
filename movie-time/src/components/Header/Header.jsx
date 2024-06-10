91e10cb13f0dd8b38c09ba666de3052090953e10
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-10 21:01:00 +0530

    header component

--- movie-time/src/components/Header/Header.jsx
+++ movie-time/src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/images/movix-logo.svg";
+import { HiOutlineSearch } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate()
+  const openSearch = () => {};
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <header className="h-[60px] bg-black flex flex-col justify-center">
+      <nav className="flex justify-between items-center px-28 py-2">
+        <div className="imgContainer">
+          <img src={Logo} className="h-12 cursor-pointer" onClick={() => navigate("/")} />
+        </div>
 
-export default Header
+        <ul className="flex gap-8 items-center text-[17px] ">
+          <li className="cursor-pointer hover:text-pink-500 transition-all ease-in-out duration-500">Movies</li>
+          <li className="cursor-pointer hover:text-pink-500 transition-all ease-in-out duration-500">Tv Shows</li>
+          <li className="cursor-pointer hover:text-pink-500 transition-all ease-in-out duration-500">
+            <HiOutlineSearch onClick={openSearch} className="text-lg" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
