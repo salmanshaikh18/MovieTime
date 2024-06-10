5cb9e7b791c9c458289b5ba5be726148f6f2246d
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-11 02:26:20 +0530

    update hero banner

--- movie-time/src/pages/Home/HeroBanner.jsx
+++ movie-time/src/pages/Home/HeroBanner.jsx
@@ -6,7 +6,7 @@ const HeroBanner = () => {
   return (
     <div className="HeroBanner relative w-full h-[450px] md:h-[700px] bg-[#04152d] flex flex-col items-center">
       <div className="backdropImage absolute h-full w-full top-0 left-0 opacity-[0.5] overflow-hidden">
-        <Img className="h-full w-full bg-cover bg-center" src={poster} />
+        <Img className="h-[75vh] sm:h-[700px] w-[100vw] bg-cover bg-center" src={poster} />
       </div>
       <div
         className="opacityLayer w-full h-[250px] absolute bottom-0 left-0"
@@ -16,17 +16,17 @@ const HeroBanner = () => {
         }}
       ></div>
       <div className="heroBannerContent sm:mt-12 flex flex-col items-center text-center text-white relative max-w-[800px] justify-center h-[100vh] leading-tight">
-        <h1 className="text-white text-[92px] font-bold">Welcome.</h1>
-        <p className="text-white text-2xl mb-4">
+        <h1 className="text-white sm:text-[92px] font-bold text-[50px]">Welcome.</h1>
+        <p className="text-white sm:text-2xl mb-4 text-[16px] px-4 text-wrap">
           Millions of movies, TV shows and people to discover. Explore now.
         </p>
-        <div className="flex items-center w-[70vw] justify-center px-10">
+        <div className="flex items-center mt-8 w-[70vw] justify-center px-2 sm:px-10">
           <input
-            className="sm:w-[90vw] sm:text-[20px] w-[60%] outline-none border-none px-0 py-[15px] rounded-l-3xl sm:px-[20px] text-black"
+            className="sm:w-[90vw] sm:text-[20px] w-[70vw] outline-none border-none px-2 py-[15px] rounded-l-3xl sm:px-[20px] text-black"
             type="text"
             placeholder="Search for a movie or tv show..."
           />
-          <button className="sm:h-[55px] h-[50px] sm:text-lg sm:w-[250px] sm:bg-red-500 w-[80px] rounded-r-3xl" style={{ background: 'linear-gradient(98.37deg, #f89e00 0.99%, #da2f68 100%)'}}>Search</button>
+          <button className="sm:h-[55px] h-[50px] px-2 sm:text-lg sm:w-[250px] sm:bg-red-500 rounded-r-3xl" style={{ background: 'linear-gradient(98.37deg, #f89e00 0.99%, #da2f68 100%)'}}>Search</button>
         </div>
       </div>
     </div>
